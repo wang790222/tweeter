32f1db2c847705537864a25dc4f8a44ed0bf8654
wang790222 <wang790222@gmail.com>
2019-01-22 19:14:20 +0000

    Implement the character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+
+
+$(document).ready(function() {
+
+  $(".new-tweet textarea").on("keyup", function() {
+
+    $(this).next().find(".counter").text(140 - this.value.length);
+    if (this.value.length > 140) {
+      $(this).next().find(".counter").addClass("red");
+    } else {
+      $(this).next().find(".counter").removeClass("red");
+    }
+  });
+});
